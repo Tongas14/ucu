6699a1da55399b7c5ab4f2da895fc003815b2b86
AgusFerreira4 <ezeferreiraagustin@gmail.com>
2025-12-01 08:22:01 -0300

    Implementacion bot, en proceso

---
+++ src/Library/Services/IBot.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Library.Services
+{
+    /// <summary>
+    /// La interfaz del Bot de Discord para usar con inyección de dependencias.
+    /// </summary>
+    public interface IBot
+    {
+        Task StartAsync(ServiceProvider services);
+
+        Task StopAsync();
+    }
+}
